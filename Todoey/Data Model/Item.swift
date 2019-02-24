09b5c5a078d6917ed01205deb8d9745cda4e958b
Zoe Olson <zoeolson@zoes-macbook-pro.local>
2019-02-24 14:54:08 -0500

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Zoe Olson on 2/24/19.
+//  Copyright © 2019 Zoe Olson. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    //  Properties of Item:
+    
+    var title : String = ""
+    var done : Bool = false
+    
+}
